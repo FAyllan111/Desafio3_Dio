2b631cb22a244473242e88cbc1aeab3df07d42b6
Ayllan Alves <franciscoayllan@gmail.com>
2024-10-17 17:18:06 -0300

    chore:inicio projeto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Celular.Models;
+
+IPhone iph = new IPhone();
+Nokia nok = new Nokia();
+
+iph.Numero(123456);
+iph.Modelo("X Pro");
+iph.IMEI(2580);
+iph.Memoria(128);
+
+nok.Numero(654321);
+nok.Modelo("Tijolão");
+nok.IMEI(1379);
+nok.Memoria(1);
